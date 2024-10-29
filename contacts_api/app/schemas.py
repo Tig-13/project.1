56b15d1314994fcc07ccd516ed03f37f41c74505
Tig-13 <139456356+Tig-13@users.noreply.github.com>
2024-10-29 16:52:28 +0100

    Add files via upload

---
+++ contacts_api/app/schemas.py
@@ -0,0 +1,28 @@
+from pydantic import BaseModel, EmailStr
+from datetime import date
+from typing import Optional
+
+class ContactBase(BaseModel):
+    first_name: str
+    last_name: str
+    email: EmailStr
+    phone: str
+    birthday: date
+    additional_info: Optional[str] = None
+
+class ContactCreate(ContactBase):
+    pass
+
+class ContactUpdate(BaseModel):
+    first_name: Optional[str] = None
+    last_name: Optional[str] = None
+    email: Optional[EmailStr] = None
+    phone: Optional[str] = None
+    birthday: Optional[date] = None
+    additional_info: Optional[str] = None
+
+class ContactInDB(ContactBase):
+    id: int
+
+    class Config:
+        orm_mode = True
